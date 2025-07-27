22da618a93a726ba0e361a8881ea3d332c988a4c
Evgenii Chetverikov <eugenechetverikov@gmail.com>
2025-07-27 15:56:40 +0300

    Added index.js - used createdAt as a trial start date

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/setTrial', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${process.env.PORT || 8080}`);
 });
